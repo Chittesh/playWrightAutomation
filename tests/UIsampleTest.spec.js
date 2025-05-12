b7e088b625540764b5553935929bcf0ae5312b52
Chittesh <45868293+Chittesh@users.noreply.github.com>
2025-05-12 22:08:08 +0530

    Update UIsampleTest.spec.js

--- tests/UIsampleTest.spec.js
+++ tests/UIsampleTest.spec.js
@@ -1,13 +1,11 @@
-const {test} = require('@playwright/test');
+const { test } = require("@playwright/test");
 
-test('First test',async ()=> 
-    {
+test("Browser fixture Context Test", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto("https://example.com");
+});
 
-
-
-}
-
-
-);
-
-
+test("Page fixture Test", async ({ page }) => {
+  await page.goto("https://example.com");
+});
